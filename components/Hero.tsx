208f603fabbd3b7b819cec8125b718a48742ea7b
Cristian Vargas <cristian.v0223@gmail.com>
2025-01-01 13:54:34 -0600

    adjusted hero to fit for codessa

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
@@ -44,7 +44,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Creative Web Magic ✨
           </p>
 
           {/**
@@ -53,17 +53,16 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Driving Growth with Custom Websites & Support"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
-          </p>
+          Chicago-based Web Design & Development Firm, offering custom websites and stress-free management so you can focus on what matters most—your business.          </p>
 
           <a href="#about">
             <MagicButton
-              title="Show my work"
+              title="See our work"
               icon={<FaLocationArrow />}
               position="right"
             />
