548ea7f239883c56091ae2aead5421b355ff1cf9
Cristian Vargas <cristian.v0223@gmail.com>
2025-01-02 17:38:34 -0600

    small change

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -94,7 +94,7 @@ export const FloatingNav = ({
                 </span>
               </Link>
             ))}
-            <button className="relative inline-flex h-10 w-auto overflow-hidden rounded-lg p-[1px] focus:outline-none">
+            <button className="relative inline-flex h-10 w-auto overflow-hidden rounded-lg p-[1.5px] focus:outline-none">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span
                 className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
@@ -157,7 +157,7 @@ export const FloatingNav = ({
                 </Link>
               ))}
               <div className="flex-col">
-                <button className="mb-2 relative inline-flex h-10 w-auto overflow-hidden rounded-lg p-[1px] focus:outline-none">
+                <button className="mb-2 relative inline-flex h-10 w-auto overflow-hidden rounded-lg p-[1.5px] focus:outline-none">
                   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                   <span
                     className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
